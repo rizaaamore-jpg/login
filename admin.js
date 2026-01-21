3fc5f4d902dd3ec9d8d03421192e24c6fde04b77
rizaaamore-jpg <rizaaamore@gmail.com>
2026-01-22 06:38:19 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,9 @@
 function toggleDark(){
   document.body.classList.toggle("dark");
 }
+
+localStorage.setItem("settingAbsen", JSON.stringify({...}))
+
 // default jam absensi
 if(!localStorage.getItem("absenTime")){
   localStorage.setItem("absenTime", JSON.stringify({
