3c685438e0342a0b8b7fb2717a3ab4d76e2f318f
rizaaamore-jpg <rizaaamore@gmail.com>
2026-01-21 19:00:30 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,18 +1,25 @@
 document.querySelector("form").addEventListener("submit", function(e){
   e.preventDefault();
 
-  const user = document.querySelector("input[type=text]").value;
-  const pass = document.querySelector(".pass-key").value;
+  const userInput = document.querySelector("input[type=text]").value;
+  const passInput = document.querySelector(".pass-key").value;
 
-  if(user === "admin" && pass === "admin123"){
-    localStorage.setItem("role","admin");
-    window.location.href = "admin.html";
-  }
-  else if(user === "siswa" && pass === "siswa123"){
-    localStorage.setItem("role","siswa");
-    window.location.href = "siswa.html";
-  }
-  else{
-    alert("Username atau password salah");
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  let user = users.find(u =>
+    u.username === userInput && u.password === passInput
+  );
+
+  if(user){
+    localStorage.setItem("role", user.role);
+    localStorage.setItem("loginUser", user.username);
+
+    if(user.role === "admin"){
+      window.location.href = "admin.html";
+    } else {
+      window.location.href = "siswa.html";
+    }
+  } else {
+    alert("Login gagal ❌");
   }
 });
